736b2c3e075df9e00b35a469173a9a1fa41d8956
Drew Weymouth <dweymouth@gmail.com>
2025-01-16 18:36:57 -0800

    rewrite using win RT, implement thumbnail

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,210 +1,158 @@
 #include "pch.h"
 
-#include "smtc.h";
+#include <chrono>
+#include <format>
+
+#include "smtc.h"
 #include <SystemMediaTransportControlsInterop.h>
-#include <windows.media.h>
-#include <windows.media.control.h>
-#include <wrl/event.h>
-#include <roapi.h>
+#include <winrt/windows.foundation.h>
+#include <winrt/windows.media.h>
+#include <winrt/windows.storage.h>
+#include <winrt/windows.storage.streams.h>
 
 #pragma comment(lib, "RuntimeObject.lib")
 
-using namespace Microsoft::WRL;
-using namespace ABI::Windows::Media;
-using ABI::Windows::Foundation::ITypedEventHandler;
-using ABI::Windows::Foundation::TimeSpan;
-using Microsoft::WRL::Wrappers::HStringReference;
+using namespace std::chrono_literals;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Media;
+using winrt::Windows::Storage::StorageFile;
+using winrt::Windows::Storage::Streams::RandomAccessStreamReference;
 
-typedef ITypedEventHandler<SystemMediaTransportControls*, SystemMediaTransportControlsButtonPressedEventArgs*> ButtonPressCallback;
-typedef ITypedEventHandler<SystemMediaTransportControls*, PlaybackPositionChangeRequestedEventArgs*> SeekCallback;
-
-int on_button_pressed(ISystemMediaTransportControls*, ISystemMediaTransportControlsButtonPressedEventArgs*);
-int on_seek(ISystemMediaTransportControls*, IPlaybackPositionChangeRequestedEventArgs*);
-
-ComPtr<ISystemMediaTransportControls> smtc;
-ComPtr<ISystemMediaTransportControlsDisplayUpdater> displayUpdater;
-
-EventRegistrationToken btnEvtTkn;
-EventRegistrationToken seekEvtTkn;
-bool btnEventsRegistered = false;
-bool seekEventsRegistered = false;
+SystemMediaTransportControls smtc{ nullptr };
 
 void (*extButtonCallback)(int);
 void (*extSeekCallback)(int);
 
-#define IFFAILRET(hr) if (FAILED((hr))) return (hr)
-
 __declspec(dllexport)
 int InitializeForWindow(HWND hwnd, void (*btnCallback)(int smtcBtn), void (*seekCallback)(int millis)) {
-    HRESULT hr;
     extButtonCallback = btnCallback;
     extSeekCallback = seekCallback;
 
-    ComPtr<ISystemMediaTransportControlsInterop> interop;
-    auto strRef = HStringReference(RuntimeClass_Windows_Media_SystemMediaTransportControls);
-    hr = Windows::Foundation::GetActivationFactory(strRef.Get(), &interop);
-    IFFAILRET(hr);
-    hr = interop->GetForWindow(hwnd, IID_PPV_ARGS(&smtc));
-    IFFAILRET(hr);
-
-    smtc->put_IsEnabled(true);
-    smtc->put_PlaybackStatus(MediaPlaybackStatus_Stopped);
-    smtc->put_IsPlayEnabled(true);
-    smtc->put_IsPauseEnabled(true);
-    smtc->put_IsPreviousEnabled(true);
-    smtc->put_IsNextEnabled(true);
-    smtc->put_IsStopEnabled(true);
-
-    hr = smtc->get_DisplayUpdater(&displayUpdater);
-    IFFAILRET(hr);
-    displayUpdater->put_Type(MediaPlaybackType_Music);
-    displayUpdater->Update();
-
-    ComPtr<ButtonPressCallback> cbBtnPressed = Callback<ButtonPressCallback>(on_button_pressed);
-    hr = smtc->add_ButtonPressed(cbBtnPressed.Get(), &btnEvtTkn);
-    IFFAILRET(hr);
-    btnEventsRegistered = true;
-
-    ComPtr<ISystemMediaTransportControls2> smtc2;
-    hr = smtc.As(&smtc2);
+    auto interop = winrt::get_activation_factory<SystemMediaTransportControls, ISystemMediaTransportControlsInterop>();
+    HRESULT hr = interop->GetForWindow(hwnd, winrt::guid_of<SystemMediaTransportControls>(), winrt::put_abi(smtc));
     if (FAILED(hr)) {
-        // seek won't be enabled, but buttons will work
-        return 0;
+        return hr;
     }
 
-    ComPtr<SeekCallback> cbSeek = Callback<SeekCallback>(on_seek);
-    smtc2->add_PlaybackPositionChangeRequested(cbSeek.Get(), &seekEvtTkn);
-    if (SUCCEEDED(hr)) {
-        seekEventsRegistered = true;
-    }
+    smtc.IsEnabled(true);
+    smtc.PlaybackStatus(MediaPlaybackStatus::Stopped);
+    smtc.IsPlayEnabled(true);
+    smtc.IsPauseEnabled(true);
+    smtc.IsPreviousEnabled(true);
+    smtc.IsNextEnabled(true);
+    smtc.IsStopEnabled(true);
 
+    auto updater = smtc.DisplayUpdater();
+    updater.Type(MediaPlaybackType::Music);
+    updater.MusicProperties().Title(winrt::to_hstring(appName));
+    updater.Update();
+
+    smtc.ButtonPressed([&](const SystemMediaTransportControls&, const SystemMediaTransportControlsButtonPressedEventArgs& args) {
+        if (!extButtonCallback) {
+            return -1;
+        }
+
+        switch (args.Button()) {
+        case SystemMediaTransportControlsButton::Play:
+            extButtonCallback(SMTCBUTTON_PLAY);
+            return 0;
+        case SystemMediaTransportControlsButton::Pause:
+            extButtonCallback(SMTCBUTTON_PAUSE);
+            return 0;
+        case SystemMediaTransportControlsButton::Stop:
+            extButtonCallback(SMTCBUTTON_STOP);
+            return 0;
+        case SystemMediaTransportControlsButton::Previous:
+            extButtonCallback(SMTCBUTTON_PREVIOUS);
+            return 0;
+        case SystemMediaTransportControlsButton::Next:
+            extButtonCallback(SMTCBUTTON_NEXT);
+            return 0;
+        }
+
+        return -1;
+    });
+
+    smtc.PlaybackPositionChangeRequested([&](const SystemMediaTransportControls&, const PlaybackPositionChangeRequestedEventArgs &args) {
+        TimeSpan pos = args.RequestedPlaybackPosition();
+        auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(pos);
+
+        if (extSeekCallback) {
+            extSeekCallback(millis.count());
+        }
+    });
+
+    return 0;
+}
+
+__declspec(dllexport)
+int SetEnabled(int enable) {
+    smtc.IsEnabled(enable > 0);
     return 0;
 }
 
 __declspec(dllexport)
 int Destroy() {
-    if (btnEventsRegistered) {
-        smtc->remove_ButtonPressed(btnEvtTkn);
-        btnEventsRegistered = false;
-    }
-    if (seekEventsRegistered) {
-        ComPtr<ISystemMediaTransportControls2> smtc2;
-        HRESULT hr = smtc.As(&smtc2);
-        if (SUCCEEDED(hr)) {
-            smtc2->remove_PlaybackPositionChangeRequested(seekEvtTkn);
-        }
-    }
-    displayUpdater->ClearAll();
-    smtc->put_IsEnabled(false);
+    smtc.IsEnabled(false);
     return 0;
 }
 
 __declspec(dllexport)
-int UpdatePlaybackState(int playback_state) {
-    if (!smtc) {
-        return -1;
-    }
-
-    MediaPlaybackStatus status = MediaPlaybackStatus_Stopped;
+int SetPlaybackState(int playback_state) {
     switch (playback_state) {
     case PLAYBACKSTATE_STOPPED:
-        status = MediaPlaybackStatus_Stopped;
-    case PLAYBACKSTATE_PAUSED:
-        status = MediaPlaybackStatus_Paused;
+        smtc.PlaybackStatus(MediaPlaybackStatus::Stopped);
+        break;
     case PLAYBACKSTATE_PLAYING:
-        status = MediaPlaybackStatus_Playing;
+        smtc.PlaybackStatus(MediaPlaybackStatus::Playing);
+        break;
+    case PLAYBACKSTATE_PAUSED:
+        smtc.PlaybackStatus(MediaPlaybackStatus::Paused);
+        break;
     }
-    return smtc->put_PlaybackStatus(status);
-}
-
-__declspec(dllexport)
-int UpdateMetadata(wchar_t* title, wchar_t* artist) {
-    HRESULT hr;
-    ComPtr<IMusicDisplayProperties> props;
-    
-    hr = displayUpdater->get_MusicProperties(&props);
-    IFFAILRET(hr);
- 
-    HSTRING hString;
-    hr = WindowsCreateString(title, lstrlenW(title), &hString);
-    IFFAILRET(hr);
-    props->put_Title(hString);
-    WindowsDeleteString(hString);
-
-    hr = WindowsCreateString(artist, lstrlenW(artist), &hString);
-    IFFAILRET(hr);
-    props->put_Artist(hString);
-    WindowsDeleteString(hString);
-
-    return displayUpdater->Update();
-}
-
-__declspec(dllexport)
-int UpdatePosition(int posMillis, int durationMillis) {
-    HRESULT hr;
-    ComPtr<ISystemMediaTransportControls2> smtc2;
-    hr = smtc.As(&smtc2);
-    IFFAILRET(hr);
-
-    ComPtr<ISystemMediaTransportControlsTimelineProperties> timeProps;
-    auto strRef = HStringReference(RuntimeClass_Windows_Media_SystemMediaTransportControlsTimelineProperties);
-    hr = RoActivateInstance(strRef.Get(), &timeProps);
-    IFFAILRET(hr);
-
-    TimeSpan timeZero = { 0 };
-    timeProps->put_MinSeekTime(timeZero);
-    timeProps->put_StartTime(timeZero);
-
-    TimeSpan curPos;
-    curPos.Duration = long(posMillis) * 1000000;
-    TimeSpan dur;
-    dur.Duration = long(durationMillis) * 1000000;
-    timeProps->put_MaxSeekTime(dur);
-    timeProps->put_EndTime(dur);
-    timeProps->put_Position(curPos);
 
     return 0;
 }
 
-int on_button_pressed(ISystemMediaTransportControls*, ISystemMediaTransportControlsButtonPressedEventArgs* args) {
-    if (!extButtonCallback) {
-        return -1;
-    }
+__declspec(dllexport)
+int SetMetadata(wchar_t* title, wchar_t* artist) {
+    auto updater = smtc.DisplayUpdater();
+    updater.Type(MediaPlaybackType::Music);
 
-    SystemMediaTransportControlsButton btn;
-    HRESULT hr = args->get_Button(&btn);
-    IFFAILRET(hr);
+    const auto& props = updater.MusicProperties();
+    props.Title(winrt::to_hstring(title));
+    props.Artist(winrt::to_hstring(artist));
 
-    switch (btn) {
-    case SystemMediaTransportControlsButton_Play:
-        extButtonCallback(SMTCBUTTON_PLAY);
-        return 0;
-    case SystemMediaTransportControlsButton_Pause:
-        extButtonCallback(SMTCBUTTON_PAUSE);
-        return 0;
-    case SystemMediaTransportControlsButton_Stop:
-        extButtonCallback(SMTCBUTTON_STOP);
-        return 0;
-    case SystemMediaTransportControlsButton_Next:
-        extButtonCallback(SMTCBUTTON_NEXT);
-        return 0;
-    case SystemMediaTransportControlsButton_Previous:
-        extButtonCallback(SMTCBUTTON_PREVIOUS);
-        return 0;
-    }
-
-    return -1;
+    updater.Update();
+    return 0;
 }
 
-int on_seek(ISystemMediaTransportControls*, IPlaybackPositionChangeRequestedEventArgs* args) {
-    if (!extSeekCallback) {
-        return -1;
-    }
-    ABI::Windows::Foundation::TimeSpan time;
-    HRESULT hr = args->get_RequestedPlaybackPosition(&time);
-    IFFAILRET(hr);
-    int millis = int(time.Duration / 1000000);
+__declspec(dllexport)
+int ClearAll() {
+    auto updater = smtc.DisplayUpdater();
+    updater.ClearAll();
+    updater.Update();
+}
 
-    extSeekCallback(millis);
+__declspec(dllexport)
+int SetPosition(int posMillis, int durationMillis) {
+    SystemMediaTransportControlsTimelineProperties timeProps;
+    timeProps.StartTime(0s);
+    timeProps.MinSeekTime(0s);
+    timeProps.Position(std::chrono::milliseconds(posMillis));
+    timeProps.MaxSeekTime(std::chrono::milliseconds(durationMillis));
+    timeProps.EndTime(std::chrono::milliseconds(durationMillis));
+    smtc.UpdateTimelineProperties(timeProps);
+
+    return 0;
+}
+
+__declspec(dllexport)
+int SetThumbnailPath(wchar_t* filepath) {
+    StorageFile storageFile = StorageFile::GetFileFromPathAsync(winrt::to_hstring(filepath)).get();
+    RandomAccessStreamReference ref = RandomAccessStreamReference::CreateFromFile(storageFile);
+    auto updater = smtc.DisplayUpdater();
+    updater.Thumbnail(ref);
+    updater.Update();
     return 0;
 }
--- smtc.h
+++ smtc.h
@@ -1,27 +1,36 @@
 #pragma once
 
 extern "C" {
-	const int SMTCBUTTON_PLAY = 1;
-	const int SMTCBUTTON_PAUSE = 2;
-	const int SMTCBUTTON_STOP = 3;
+	const int SMTCBUTTON_PLAY = 0;
+	const int SMTCBUTTON_PAUSE = 1;
+	const int SMTCBUTTON_STOP = 2;
 	const int SMTCBUTTON_PREVIOUS = 4;
 	const int SMTCBUTTON_NEXT = 5;
 
-	const int PLAYBACKSTATE_STOPPED = 0;
-	const int PLAYBACKSTATE_PLAYING = 1;
-	const int PLAYBACKSTATE_PAUSED = 2;
+	const int PLAYBACKSTATE_STOPPED = 2;
+	const int PLAYBACKSTATE_PLAYING = 3;
+	const int PLAYBACKSTATE_PAUSED = 4;
 
 	__declspec(dllexport)
 		int InitializeForWindow(HWND hwnd, void (*btnCallback)(int smtcBtn), void (*seekCallback)(int millis));
 
 	__declspec(dllexport)
-		int UpdatePlaybackState(int playback_state);
+		int SetEnabled(int enable);
 
 	__declspec(dllexport)
-		int UpdateMetadata(wchar_t* title, wchar_t* artist);
+		int SetPlaybackState(int playback_state);
 
 	__declspec(dllexport)
-		int UpdatePosition(int posMillis, int durationMillis);
+		int SetMetadata(wchar_t* title, wchar_t* artist);
+
+	__declspec(dllexport)
+		int SetPosition(int posMillis, int durationMillis);
+
+	__declspec(dllexport)
+		int SetThumbnailPath(wchar_t* filepath);
+
+	__declspec(dllexport)
+		int ClearAll();
 
 	__declspec(dllexport)
 		int Destroy();
